5709abda2e022ee31444d0ef17c60c575b4d47c5
Your Name <karoliina.mietola@helsinki.fi>
2018-12-09 17:50:05 +0200

    teht4

---
+++ QueryLanguage/src/main/java/statistics/Querybuilder.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistics;
+
+import java.util.ArrayList;
+import statistics.matcher.All;
+import statistics.matcher.And;
+import statistics.matcher.HasAtLeast;
+import statistics.matcher.HasFewerThan;
+import statistics.matcher.Matcher;
+import statistics.matcher.PlaysIn;
+
+/**
+ *
+ * @author kape
+ */
+public class Querybuilder {
+
+    Matcher matcher;
+
+    public Querybuilder() {
+        this.matcher = new All();
+
+    }
+
+    public Matcher build() {
+        return this.matcher;
+    }
+
+    public Querybuilder playsIn(String attribute) {
+        matcher = new And(new PlaysIn(attribute), matcher);
+        return this;
+    }
+
+    public Querybuilder hasAtLeast(int value, String category) {
+        matcher = new And(new HasAtLeast(value, category), matcher);
+        return this;
+    }
+
+    public Querybuilder hasFewerThan(int value, String category) {
+        matcher = new And(new HasFewerThan(value, category), matcher);
+        return this;
+    }
+
+}
